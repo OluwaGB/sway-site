b929534517a2679df9007787af5cc7c7e6b6f669
I T_code <oluwagbotemioyeboade@gmail.com>
2024-03-23 23:53:33 +0100

    Image component

---
+++ src/Img.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import img3 from "./img/image.jpg"
+import img2 from "./img/digital-service2 (2).png"
+
+const Img=( )=>{
+    return(<div className='wrap1'>
+    <img id="img" src={img2}/>
+    </div>)
+}
+export default Img
